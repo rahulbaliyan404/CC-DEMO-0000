8d470c18ce56865d2fb0aea44dbc1f8fbe4bfce7
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2021-12-16 16:44:44 +0530

    This is first commit 16-12-2021.

--- CC-Demo-0000/src/main/java/com/home/interview/Demo.java
+++ CC-Demo-0000/src/main/java/com/home/interview/Demo.java
@@ -1,11 +1,13 @@
 package com.home.interview;
 
 public class Demo {
-	 Employee e;
-
+	 
 	public static void main(String[] args) {
+		Employee e = null;
+		System.out.println(e.getName());
 		Employee e1 = new Employee();
 		System.out.println(e1.getName());
+		
 	}
 
 }
