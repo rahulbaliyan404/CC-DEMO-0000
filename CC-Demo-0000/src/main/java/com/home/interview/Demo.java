76e4a713adf0ec745817d54c04031902320d072b
rahulbaliyan@gmail.com <rahulbaliyan@gmail.com>
2021-12-18 22:44:08 +0530

    This is master branch commit

--- CC-Demo-0000/src/main/java/com/home/interview/Demo.java
+++ CC-Demo-0000/src/main/java/com/home/interview/Demo.java
@@ -3,8 +3,8 @@ package com.home.interview;
 public class Demo {
 	 
 	public static void main(String[] args) {
-		//Employee e = null;
-		//System.out.println(e.getName());
+		Employee e = null;
+		System.out.println(e.getName());
 		Employee e1 = new Employee();
 		System.out.println(e1.getName());
 		
